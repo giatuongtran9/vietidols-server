25261a950331b1381995a261d468c440db833dbf
Gia Tuong Tran <trangiatuong999@gmail.com>
2021-03-06 00:03:45 -0500

    add dotenv

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
 
 import authRoutes from './routes/auth.js'
 
 const app = express()
 app.use(cors())
+
+dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
